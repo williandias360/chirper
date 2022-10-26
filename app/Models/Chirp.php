860f2cd8d82bb888d8ce21db0c3dd249a5343e41
Willian D. Brito <willian.dias360@gmail.com>
2022-10-26 14:09:55 -0300

    Init Commit

---
+++ app/Models/Chirp.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\User;
+use App\Events\ChirpCreated;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Chirp extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'message',
+    ];
+
+    protected $dispatchesEvents = [
+        'created' => ChirpCreated::class,
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
